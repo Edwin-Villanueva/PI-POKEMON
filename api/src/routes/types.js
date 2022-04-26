1a5ce555c7f03de8a93b44c322780eb516cba494
Edwin Villanueva <edwinv.dev92@gmail.com>
2022-04-26 20:02:31 -0300

    avanzando con las rutas del back v3

--- api/src/routes/types.js
+++ api/src/routes/types.js
@@ -5,24 +5,49 @@ const { URL_TYPE } = process.env;
 const { Type } = require("../db")
 const router = Router();
 
+
 router.get("/",async(req,res,next)=>{
-    let types = await axios.get(URL_TYPE);
-    arrayTypesUrl= types.data.results.map((type)=>{
-        return axios.get(type.url);
-
-    })
-    Promise.all(arrayTypesUrl)
-    .then(resultado =>{
-
-        typesFilt = resultado.map((type)=>{
-            let nuevoType={
-                name : type.data.name,
-                id : type.data.id
+    try {
+        let typesDB= await Type.findAll()
+        if(typesDB.length !== 0){
+            console.log("entre al if")
+            res.send(typesDB);
+        }
+        else{
+            try {
+                let types = await axios.get(URL_TYPE);
+                arrayTypesUrl= types.data.results.map((type)=>{
+                    return axios.get(type.url);
+                })
+                Promise.all(arrayTypesUrl)
+                .then(resultado =>{
+        
+                    typesFilt = resultado.map((type)=>{
+                        let nuevoType={
+                            name : type.data.name.toLowerCase(),
+                            id : type.data.id
+                        }
+                        return nuevoType;  
+                        
+                    });
+                    console.log("entre al else");
+                    typesFilt.forEach(async(type) => {
+                        await Type.create({
+                            type// si esto tira error desglosar el tipe en name y en id
+                        })
+                    });
+                    res.send(typesFilt);
+                })
+                
+            } catch (error) {
+                res.send(error);
             }
-            return nuevoType;  
-            
-        })
-        res.send(typesFilt);
-    })
+
+        }
+        
+    } catch (error) {
+        res.send("error en DB");
+    }
+    
 })
 module.exports = router;
