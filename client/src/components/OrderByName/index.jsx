91b87b0bb1d40f2c26faaf4b106fcb3891e7a5ac
Edwin Villanueva <edwinv.dev92@gmail.com>
2022-05-04 23:14:15 -0300

    avanzando con el front botones, filtrados,ect v1

---
+++ client/src/components/OrderByName/index.jsx
@@ -0,0 +1,15 @@
+
+
+export default function OrderByName(){
+
+    return(
+        <div>
+            <span>OrdenarPorNombre</span>
+            <select name="" id="">
+                <option value="">seleccionar</option>
+                <option value="">ascendente-AZ</option>
+                <option value="">descendente-ZA</option>
+            </select>
+        </div>
+    )
+}
